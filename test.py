2a79146a7694f6c3983ed403762129f45f53da00
Hajime Murao <2620990+hjmr@users.noreply.github.com>
2018-11-08 14:31:28 +0900

    Modify to use argparse.

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import sys
+import argparse
+
 import numpy as np
 import chainer
 from chainer import serializers
@@ -7,12 +9,23 @@ from MyNet import MyNet
 from read_data import read_one_image
 
 
+def parse_arg():
+    parser = argparse.ArgumentParser(description='Test the trained convolution network.')
+    parser.add_argument('-m', '--model', type=str, nargs=1,
+                        help='specify a file name of a trained model.')
+    parser.add_argument('test_images', type=str, nargs='+',
+                        help='file name(s) of test image(s).')
+    return parser.parse_args()
+
+
+args = parse_arg()
+
 model = MyNet()
-serializers.load_npz('models/mynet.model', model)
+serializers.load_npz(args.model[0], model)
 
 test_images = []
-for i in range(1, len(sys.argv)):
-    test_images.append(read_one_image(sys.argv[i]))
+for img in args.test_images:
+    test_images.append(read_one_image(img))
 
 for i in range(len(test_images)):
     img = np.array([test_images[i]], dtype=np.float32)
--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import argparse
+
 import numpy as np
 from chainer import optimizers, serializers
 
@@ -6,7 +8,19 @@ from MyNet import MyNet
 from read_data import read_data
 
 
-train_images, train_labels = read_data('train.txt')
+def parse_arg():
+    parser = argparse.ArgumentParser(description='Train convolution network.')
+    parser.add_argument('-m', '--model', type=str,
+                        help='specify a file name where trained model will be saved to.')
+    parser.add_argument('-l', '--log_interval', type=int, default=10,
+                        help='An interval of displaying a log.')
+    parser.add_argument('train_data', type=str, nargs=1,
+                        help='a training data where the file name of an image and its class are on each of the lines.')
+    return parser.parse_args()
+
+
+args = parse_arg()
+train_images, train_labels = read_data(args.train_data[0])
 
 model = MyNet()
 optimizer = optimizers.Adam()
@@ -29,8 +43,8 @@ for epoch in range(100):
 
         accum_loss = loss if accum_loss is None else accum_loss + loss
 
-    if epoch % 10 == 0:
+    if epoch % args.log_interval == 0:
         print(epoch, accum_loss.data)
 
-outfile = "models/mynet.model"
-serializers.save_npz(outfile, model)
+if args.model is not None:
+    serializers.save_npz(args.model, model)
